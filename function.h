056383a10eb822520cddcea934522a7d11bd570b
ls3k6 <90429544+ls3k6@users.noreply.github.com>
2022-12-09 17:39:39 -0600

    Create function.h

---
+++ function.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+#include <queue>
+#include <map>
+using namespace std;
+
+void queuePrint(queue<int> q);
+void queueChange(queue<int>& q);
+bool palindromeCheck(string b, int start, int end);
+void outputMap(map<string, string> m);
+void checkMap(map<string, string> m, string target);
